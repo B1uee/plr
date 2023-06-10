cb977e0ca84afa62810667f896772881a0dd444b
B1ue_Han <44899660+B1uee@users.noreply.github.com>
2023-06-10 21:34:34 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -6,13 +6,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from torch.utils.tensorboard import SummaryWriter
+import math
+from datetime import date, datetime
+import numpy.linalg as LA
 
-def draw_plot(data,plot_title):
+def draw_plot(data, segments, plot_title):
     plot(data[:,0],data[:,1],alpha=0.8,color='red')
+    plot(segments[:,0],segments[:,1],alpha=0.8,color='blue')
     title(plot_title)
-    xlabel("Samples")
-    ylabel("Signal")
-    xlim((0,len(data)-1))
+    xlabel("time")
+    ylabel("cwnd")
+
 def draw_segments(segments):
     ax = gca()
     for segment in segments:
@@ -77,29 +81,108 @@ def topdownsegment(max_error=0.001):
                     save_path = os.getcwd()+'/test/'+alg
                     if not os.path.exists(save_path):
                         os.makedirs(save_path)
-                    np.savetxt(os.path.join(save_path,file+'.txt'),np.asarray(output),fmt='%.8f %.6f')
+                    np.savetxt(os.path.join(save_path,file+'_2.txt'),np.asarray(output),fmt='%.8f %.6f')
                 #np.savetxt('/home/ml4net/LJH/LJH/PLR/out_speed/'+alg+'/'+file,np.asarray(output),fmt='%.8f %.6f')
 
-def load_data(path, writer):
+def load_data(path):
     with open(path, 'r') as f:
         file_lines = f.readlines()
     data=[]   #输入xy二维数据
-    for line in file_lines:
+    for line in file_lines[0:2000]:
         if line.strip()!='0.0000000 0.0000000':
             tmp=line.strip().split(' ')
             x=float(tmp[0])
             y=float(tmp[1])
-            writer.add_scalar("raw_data", y, x)
             data.append([x,y])
     data=np.asarray(data)
     return data
 
+def get_TurnPoint(data, angle_thred):
+    ''' 
+    :paras data:时间序列数据
+    :paras angle_thred: 判定其是否为转折点的角度阈值
+ 
+    :return turnPoints: 预选的转折点
+    '''
+    turnPoints = []
+    for index in range(1, len(data)-1):
+        # 这里存在一个隐性的 时间序列采样是否均匀 的问题 
+        # 这里如果时间采样不均匀，记得将 atan() 中的数据除以对应时间差
+        theta1 = math.atan((data[index][1] - data[index - 1][1])/(data[index][0] - data[index - 1][0])) * (180 / math.pi)
+        theta2 = math.atan((data[index + 1][1] - data[index][1])/(data[index + 1][0] - data[index][0])) * (180 / math.pi)
+        if theta1 * theta2 < 0:
+            alpha = abs(theta1) + abs(theta2)
+        else:
+            alpha = abs(theta1 - theta2)
+        if alpha > angle_thred:
+            turnPoints.append(index)
+    turnPoints.insert(0, 0)
+    turnPoints.append(len(data)-1)
+    return turnPoints
+
+def merge_cost(data, merge_start, merge_end):
+    ''' 
+    本方法使用 least-squares solution to a linear matrix equation 方法进行直线拟合。
+    同时获得拟合误差。
+    调用 numpy.linalg.lstsq() 方法
+    :param data: 时间序列数据
+    :param merge_start: 合并起始点
+    :param merge_end: 合并终点
+ 
+    :return error: 合并产生的误差
+    '''
+    x = np.arange(merge_start, merge_end+1) # +1 的原因是 np.arange 为左闭右开
+    y = np.array(data[merge_start:merge_end+1])
+    A = np.ones((len(x), 2), np.float64)
+    A[:,0] = x
+    try:
+        (result, residual, rank, s) = LA.lstsq(A, y[:,1], rcond=None)
+        error = math.sqrt(residual[0])
+
+    except np.linalg.LinAlgError:
+        error = 0.0
+    except IndexError:
+        error = 0.0
+ 
+    return error
+
+def bottomUp(data, turn_points, error_thred, error_estimator=merge_cost):
+    ''' 
+    :param data: 时间序列数据
+    :param turn_points: 转折点数据
+    :param error_thred: 融合过程中的误差阈值, 超过阈值后, 融合停止
+    :param error_estimator: 融合过程中的误差估计方法
+ 
+    :return segments
+    '''
+    index = 0
+    segments = [turn_points[0]] # 加入起始的开始点
+    len_turnPoints = len(turn_points) - 1
+ 
+    while index < len_turnPoints:
+        start = index
+        end = index + 1
+        while error_estimator(data, turn_points[start], turn_points[end]) < error_thred:
+            end += 1
+            if not (end < len_turnPoints): break
+        segments.append(turn_points[end])
+        index = end
+ 
+    return segments
+
+def new_segement():
+    #writer = SummaryWriter("logs/"+datetime.now().strftime("%m-%d-%H_%M_%S"))
+    raw_data = load_data("trace/newreno/5/cwnd.txt")
+    turn_points = get_TurnPoint(raw_data, 1e-6)
+    segments = bottomUp(raw_data, turn_points, 1e-13, merge_cost)
+    draw_plot(raw_data, raw_data[segments], "RawData & Segments")
+    plt.savefig('fig_test.png')
 
 if __name__ == "__main__":
-    # raw_data = load_data("trace/newreno/5/cwnd.txt")
+    new_segement()
     # pre_data = load_data("test/newreno/5.txt")
 
-    topdownsegment(0.0001)
+    #topdownsegment(0.0001)
     # with open("test/newreno/5.txt", 'r') as f:
     #     file_lines = f.readlines()
     # data=[]   #输入xy二维数据
