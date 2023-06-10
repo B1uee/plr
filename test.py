e03c4a063fd5cfe9dfae51eced02a960511e7b55
B1uee <379264406@qq.com>
2023-06-10 15:38:48 +0800

    modified:   test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import fit
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-
+from torch.utils.tensorboard import SummaryWriter
 
 def draw_plot(data,plot_title):
     plot(data[:,0],data[:,1],alpha=0.8,color='red')
@@ -25,20 +25,35 @@ def topdownsegment(max_error=0.001):
     for alg in ['newreno']:
         #path='/home/ml4net/LJH/LJH/contrastive-predictive-cc/data/out/'+alg  
         path = os.getcwd() + "/trace/" + alg
+        writer = SummaryWriter("logs")
         files = os.listdir(path)
         for file in files:
             tmp_path = "{}/{}".format(path, file)
             tmp_files = os.listdir(tmp_path)
             for tmp_file in tmp_files:
                 if tmp_file.endswith('.txt'):
-                    data, speeds_all, segments_all = load_data(os.path.join(path,file+'/'+tmp_file), max_error)
+                    data = load_data(os.path.join(path,file+'/'+tmp_file))
+                    speeds=[]
+                    segments_all=[]
+                    speeds_all=[]
+                    start=0
+                    while start<=len(data):
+                        end=min(start+5000,len(data)-1)
+                        input_data=data[start:end]
+                        start+=5000
+                        segments,speeds = segment.topdownsegment(input_data, fit.regression, fit.sumsquared_error, max_error,speeds=speeds)
+                        if start==0:
+                            segments_all=segments
+                            speeds_all=speeds
+                        else:
+                            segments_all+=segments
+                            speeds_all+=speeds
                     '''
                     draw_plot(data,"Sliding window with regression")
                     plt.savefig('fig_test_1.png')
                     draw_segments(segments)
                     plt.savefig('fig_test.png')
                     '''
-
                     #按照输入，匹配每个值的斜率进行输出
                     start_index=0
                     output=[]
@@ -52,48 +67,38 @@ def topdownsegment(max_error=0.001):
                         if x>=segments_all[start_index][0] and x<=segments_all[start_index][2]:
                             speed_x=speeds_all[start_index][0]
                             output.append([x,speed_x])
+                            writer.add_scalar("pre_data", speed_x, x)
                             i+=1
                         else:
                             start_index+=1
                             if start_index>=len(segments_all):
                                 break
+                    writer.close()
                     save_path = os.getcwd()+'/test/'+alg
                     if not os.path.exists(save_path):
                         os.makedirs(save_path)
                     np.savetxt(os.path.join(save_path,file+'.txt'),np.asarray(output),fmt='%.8f %.6f')
                 #np.savetxt('/home/ml4net/LJH/LJH/PLR/out_speed/'+alg+'/'+file,np.asarray(output),fmt='%.8f %.6f')
 
-def load_data(path, max_error):
+def load_data(path):
     with open(path, 'r') as f:
         file_lines = f.readlines()
     data=[]   #输入xy二维数据
-    for line in file_lines[0:2000]:
+    for line in file_lines:
         if line.strip()!='0.0000000 0.0000000':
             tmp=line.strip().split(' ')
             x=float(tmp[0])
             y=float(tmp[1])
+            writer.add_scalar("raw_data", y, x)
             data.append([x,y])
     data=np.asarray(data)
-    speeds=[]
-    segments_all=[]
-    speeds_all=[]
-    start=0
-    while start<=len(data):
-        end=min(start+5000,len(data)-1)
-        input_data=data[start:end]
-        start+=5000
-        segments,speeds = segment.topdownsegment(input_data, fit.regression, fit.sumsquared_error, max_error,speeds=speeds)
-        if start==0:
-            segments_all=segments
-            speeds_all=speeds
-        else:
-            segments_all+=segments
-            speeds_all+=speeds
-    return data, speeds_all, segments_all
+    return data
 
 
 if __name__ == "__main__":
-    #data, _,_, = load_data()
+    # raw_data = load_data("trace/newreno/5/cwnd.txt")
+    # pre_data = load_data("test/newreno/5.txt")
+
     topdownsegment(0.0001)
     # with open("test/newreno/5.txt", 'r') as f:
     #     file_lines = f.readlines()
